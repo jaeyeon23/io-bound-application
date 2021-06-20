2c7253847878e6acc83ca10462d96f435727d2d7
jaeyeon.park <catblues282@gmail.com>
2021-06-20 17:11:17 +0900

    첫 페이지 캐싱 처리

--- src/main/java/class101/foo/io/IoApplication.java
+++ src/main/java/class101/foo/io/IoApplication.java
@@ -2,7 +2,9 @@ package class101.foo.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling		// 크론 표현식을 사용하기 위한 코드
 @SpringBootApplication
 public class IoApplication {
 
